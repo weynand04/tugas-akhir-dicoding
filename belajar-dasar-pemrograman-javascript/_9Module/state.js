a395f91da25c2f9f28e632a74520d06c91357ff8
Weynand04 <wamaerkabor@gmail.com>
2024-04-16 07:17:29 +0700

    Update selesai coruse javascript

---
+++ belajar-dasar-pemrograman-javascript/_9Module/state.js
@@ -0,0 +1,8 @@
+const coffeeStock = {
+  arabica: 100,
+  robusta: 150,
+  liberica: 200,
+};
+const isCoffeeMachineReady = true;
+
+module.exports = { coffeeStock, isCoffeeMachineReady };
---
+++ belajar-dasar-pemrograman-javascript/wolf.js
@@ -0,0 +1,13 @@
+class Wolf {
+  constructor() {
+    this.strength = Math.floor(Math.random() * 100);
+  }
+
+  howl() {
+    return "Auuuuuuuuu";
+  }
+}
+
+// TODO 2
+
+export default Wolf();
